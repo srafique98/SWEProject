40dae75b6b0a556602045ee7b60c70af61fda699
David Leavenworth <dleavenworth3@gmail.com>
2022-03-31 00:10:35 -0700

    Added a unit test

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,13 +1,17 @@
-from db_client import DB_Client
+from src.db_client import DB_Client
+from src.Window import Window
+
 
 def test_first():
     return True
 
-def test_db_connection():
+
+def test_dbConnection():
     newDB = DB_Client(True)
     return newDB.intitialized
 
-def test_db_location_filter():
+
+def test_dbLocationFilter():
     newDB = DB_Client(True)
     try:
         newDB.fil_location("Berkeley")
@@ -15,10 +19,21 @@ def test_db_location_filter():
     except:
         return False
 
-def test_db_profession_filter():
+
+def test_DbProfessionFilter():
     newDB = DB_Client(True)
     try:
         newDB.fil_sect_profession("Engineer")
         return True
     except:
-        return False 
+        return False
+
+
+def test_windowInit():
+    testFile = "../ui/mainmenu.ui"
+
+    try:
+        testWindow = Window()
+        testWindow.windowInit(testFile)
+    except SystemExit:
+        return False
