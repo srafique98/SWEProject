478e819fd4672716aa4d236b667eff64575b369d
srafique98 <shanrafique98@gmail.com>
2022-04-04 02:35:28 -0700

    Tidied things up in Listing.py

--- src/Listing.py
+++ src/Listing.py
@@ -1,37 +1,24 @@
-from sqlite3 import Cursor
 from Window import Window
 from db_client import DB_Client
-
-import sys
 from PySide6.QtWidgets import QApplication, QPushButton
 from PySide6.QtCore import Slot
 
-class Listing(Window):  # inheritance from window class
+class Listing(Window):
     def __init__(self):
         uiFile = "../ui/mainwindow.ui"
-        super().__init__()      # implicitly calling the parent class
+        super().__init__()
         self.window = super().windowInit(uiFile, self)
         # self.window.show()
 
         self.connection = DB_Client(True)
-        self.job_button = self.findChild(QPushButton,"pushButton")
+        self.jobButton = self.findChild(QPushButton,"pushButton") # From mainwindow.ui
         self.cursor = self.connection.dbCollection.find({})
 
         # DO Something
-        self.job_button.clicked.connect(self.display_a_job)
+        self.jobButton.clicked.connect(self.displayAJob)
 
     @Slot()
-    def display_a_job(self):
-        obj = next(self.cursor, None)   # if no next then make obj = None
+    def displayAJob(self):
+        obj = next(self.cursor, None)
         if obj:
             print(obj)
-        # for document in self.cursor:
-        #     print(document)
-        #     break
-        # print(self.cursor.hasNext()) self.cursor = self.connection.dbCollection
-
-
-app = QApplication(sys.argv)
-window = Listing()
-window.show()
-sys.exit(app.exec())
