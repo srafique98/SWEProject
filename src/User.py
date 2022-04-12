2a3bbc83d5f5f5e77669e67e889a52a974f43732
SeanNH <34140944+SeanNH@users.noreply.github.com>
2022-04-12 14:03:16 -0700

    Added to user sign-in function and created functions for modifying user data

--- src/User.py
+++ src/User.py
@@ -18,11 +18,18 @@ class User:
     }
 
     def __init__(self):
-        local_hostname = socket.gethostname()
-        self.profile_info["host_addres"] = local_hostname
-        self.profile_info["ip"] = socket.gethostbyname(local_hostname)
+        
+        try:
+            local_hostname = socket.gethostname()
+            self.profile_info["host_addres"] = local_hostname
+            self.profile_info["ip"] = socket.gethostbyname(local_hostname)
+        except:
+            print("Unable to get hostname or ip address")
         self.profile_info["last_logged"] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
+#   *****************************************
+
+# SIGN IN
     def validateUserLogin(self, email, pw):
         db_obj = db_cli.DB_Client(True,"Jobs","users")
         doc_cursor = db_obj.get_all_users()
@@ -33,7 +40,10 @@ class User:
                 self.profile_info["username"] = usr_doc["email"]
                 self.profile_info["u_id"] = usr_doc["u_id"]
                 self.profile_info["fullname"] = usr_doc["first_name"] + " " + usr_doc["last_name"]
-                
+                user_query = {"u_id":self.profile_info["u_id"]}
+                user_insert = ({"$set":{"date_last_signin":self.profile_info["last_logged"]}},{"$set":{"last_ip":self.profile_info["ip"]}},{"$set":{"ip_hostname":self.profile_info["host_address"]}})
+                for db_insert in user_insert:
+                    db_obj.dbCollection.update_one(user_query,db_insert)
                 self.signed_in = True
                 return True
         return False    
@@ -54,7 +64,18 @@ class User:
         high = None
         for field in largest_u_id:
             high = field["u_id"]
-        user_insert = {"u_id":high+1, "first_name":f_name, "last_name":l_name,"email":email,"Password":pw}
+        user_insert = {"u_id":high+1, "first_name":f_name, "last_name":l_name,"email":email,"Password":pw,"date_last_signin":self.profile_info["last_logged"],"last_ip":self.profile_info["ip"]}
         db_obj.dbCollection.insert_one(user_insert)
 
 #   *****************************************
+
+# CHANGE USER SPECIFIC DATA
+    # Pass in the 'field' that you want to change
+    # Pass in the 'value' that you would like to change it to
+    def changeUserData(self, field, value): # self, string, (int or string)
+        db_obj = db_cli.DB_Client(True, "Jobs", "users")
+        user_query = {"u_id": self.profile_info["u_id"] }
+        user_update = {"$set":{ field : value }}
+        db_obj.dbCollection.update_one(user_query, user_update)
+        print("Changed " + self.profile_info["fullname"] + " " + field + " to " + ("*"*len(value)))
+
