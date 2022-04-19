597cd76cef882a4c67dca2f984e1ef9ee4ad6497
srafique98 <shanrafique98@gmail.com>
2022-04-19 14:16:57 -0700

    returns resume full path

--- src/Profile.py
+++ src/Profile.py
@@ -1,6 +1,9 @@
+from argparse import FileType
 from src.Window import Window
 from src.User import User 
 from PySide6.QtWidgets import *
+from PySide6 import QtCore, QtGui
+import sys
 
 
 class Profile(Window):
@@ -24,6 +27,12 @@ class Profile(Window):
         self.saveSummary = self.findChild(QPushButton, "saveSummary")
         self.saveSummary.clicked.connect(self.saveData)
 
+        self.resumeButton = self.findChild(QPushButton, "pushButton")
+        self.filePath = self.findChild(QLabel, "label_6")
+
+        # click upload resume button
+        self.resumePath = self.resumeButton.clicked.connect(self.getResumePath)
+
         self.parentWindow = parentWindow
 
     def testBack(self):
@@ -38,5 +47,16 @@ class Profile(Window):
         self.summary.setReadOnly(True)
         #tempUser.updateLocalInfo()
         tempUser.changeUserData("summary", self.summary.toPlainText())
+
+    def getResumePath(self):
+        self.app = QApplication.instance()
+        self.app.setQuitOnLastWindowClosed(False)
+        resumeFullPath, resumeType = QFileDialog.getOpenFileNames(self)
+        if resumeFullPath:
+            # print("--", resumeFullPath)
+            self.filePath.setText(str(resumeFullPath))
+        self.app.setQuitOnLastWindowClosed(True)
+        return resumeFullPath
+        
         
    
